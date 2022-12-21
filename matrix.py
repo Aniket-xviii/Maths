1157ab6516100b42f233bdb1144dce44542d020f
Aniket Sharma <as1475327@gmail.com>
2022-12-21 10:19:10 +0530

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,5 @@
+import numpy as np
+import math
+mat1 = np.matrix(input("Write elements separated by spaces and separate rows with semi-colon: "))
+print("Matrix 1 is: ", "\n", mat1)
+print("Transpose of Matrix 1 is: ", "\n", mat1.T)
